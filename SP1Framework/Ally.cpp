fbd475264a3ffcf5562c6e95a6e9bad293e1c48f
IisAndrew <andrewchanjw17@gmail.com>
2020-08-20 14:45:28 +0800

    skrr skrr

---
+++ SP1Framework/Ally.cpp
@@ -0,0 +1,48 @@
+#include "Ally.h"
+
+Ally::Ally()
+{
+	name = 'O';
+}
+
+void Ally::setAlly()
+{
+	this->SetD(x);
+	this->SetH(50);
+	this->SetX(0);
+	this->SetY(0);
+}
+
+int Ally::getDmg()
+{
+	return this->GetD();
+}
+
+int Ally::getHealth()
+{
+	return this->GetH();
+}
+
+int Ally::getXCoords()
+{
+	return this->GetX();
+}
+
+int Ally::getYCoords()
+{
+	return this->GetY();
+}
+
+char Ally::getNamed()
+{
+	return this->GetName();
+}
+
+int Ally::getAlly()
+{
+	this->getDmg();
+	this->getHealth();
+	this->getXCoords();
+	this->getYCoords();
+	this->getNamed();
+}
--- SP1Framework/MutantWasp.h
+++ SP1Framework/MutantWasp.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Enemy.h"
+#include "Poison.h"
 
 class MutantWasp : public Enemy
 {
