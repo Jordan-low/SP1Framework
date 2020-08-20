af7b20190c1b03d1c87403b1d1d678719c645421
x3n0vial <69580639+x3n0vial@users.noreply.github.com>
2020-08-20 12:00:37 +0800

    cutscenes animate function and next dialogue function

--- SP1Framework/Cutscenes.cpp
+++ SP1Framework/Cutscenes.cpp
@@ -1,1 +1,26 @@
 #include "Cutscenes.h"
+
+
+void Cutscenes::animate()
+{
+	Border(g_Console);
+	std::this_thread::sleep_for(std::chrono::milliseconds(300));
+	system("cls");
+}
+
+void Cutscenes::nextdialogue()
+{
+	system("pause");
+	system("cls");
+	Border(g_Console);
+}
+
+char Cutscenes::getgrid(int i, int j, char c)
+{
+	return (Grid[i][j] = c);
+}
+
+char Cutscenes::cleargrid(int i, int j)
+{
+	return (Grid[i][j] = ' ');
+}
--- SP1Framework/Cutscenes.h
+++ SP1Framework/Cutscenes.h
@@ -1,7 +1,17 @@
 #pragma once
 #include "Map.h"
+#include "Framework\console.h"
+#include <iostream>
+#include <stdlib.h>
+#include <chrono>
+#include <thread>
+
 class Cutscenes :
     public Map
 {
+    void animate();
+    void nextdialogue();
+    char getgrid(int i, int j, char c);
+    char cleargrid(int i, int j);
 };
 
