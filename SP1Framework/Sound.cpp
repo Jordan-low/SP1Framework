aece881217dccbc19a6ec00777bfe385d73f8f4a
Nicole <202435L@mymail.nyp.edu.sg>
2020-08-28 10:58:30 +0800

    push to pull

--- SP1Framework/Sound.cpp
+++ SP1Framework/Sound.cpp
@@ -1,7 +1,12 @@
 #include "Sound.h"
 #include <windows.h>
+#include <iostream>
+
+using namespace std;
 
 void Sound::sound()
 {
-	bool music = PlaySound(L"../SP1Framework/sound.wav", NULL, SND_FILENAME | SND_ASYNC);
+
+	bool music = PlaySound(TEXT("C:\\Users\\chong\\Documents\\School Stuff\\DM2196-STUDIO PROJECT 1\\SP1\\SP1Framework\\SP1Framework\\sound.wav"), NULL, SND_ASYNC);
+	
 }
